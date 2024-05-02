70c40298802b298ad09eb981ad6c5b813b815c3d
ronlechiht <ronlechi1998ht@gamil.com>
2024-05-02 10:45:36 +0700

    feat: show the past moves

--- tic-tac-toe-project/src/App.tsx
+++ tic-tac-toe-project/src/App.tsx
@@ -5,10 +5,7 @@ function Square({value, onSquareClick}: {value: string, onSquareClick: MouseEven
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null))
-
+function Board({ xIsNext, squares, onPlay }: {xIsNext: boolean, squares: any[], onPlay: CallableFunction}) {
   function handleClick(i: number) {
     if (squares[i] || calculateWinner(squares)) return;
     const nextSquares = squares.slice();
@@ -18,8 +15,7 @@ function Board() {
       nextSquares[i] = "O";
     }
     
-    setSquares(nextSquares)
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares)
   }
 
   const winner = calculateWinner(squares);
@@ -72,4 +68,48 @@ function calculateWinner(squares: any[]) {
   return null;
 }
 
-export default Board
+function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares: any[]) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove: number) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove %2 === 0)
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+export default Game
