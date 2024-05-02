13156ab47b8c2237b53977077fb0089a1468125f
ronlechiht <ronlechi1998ht@gamil.com>
2024-05-02 10:50:21 +0700

    feat: remove xIsNext state

--- tic-tac-toe-project/src/App.tsx
+++ tic-tac-toe-project/src/App.tsx
@@ -69,21 +69,19 @@ function calculateWinner(squares: any[]) {
 }
 
 function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares: any[]) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove: number) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove %2 === 0)
   }
 
   const moves = history.map((squares, move) => {
