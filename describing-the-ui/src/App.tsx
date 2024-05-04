d1bb3b3af8dcdfa1ba27248965f025493e66e156
ronlechiht <ronlechi1998ht@gmail.com>
2024-05-04 09:55:40 +0700

    Importing and Exporting components code example

--- describing-the-ui/src/App.tsx
+++ describing-the-ui/src/App.tsx
@@ -1,21 +1,7 @@
-function Profile() {
+import Gallery from "./Gallery";
+
+export default function App () {
   return (
-    <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
+    <Gallery />
   )
 }
-
-function Gallery() {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
-  )
-}
-
-export default Gallery
