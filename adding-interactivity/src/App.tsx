4143c70e09f582abcb7f7511ee8640db49240186
ronlechiht <ronlechi1998ht@gmail.com>
2024-05-06 15:44:40 +0700

    state: a component's memory code example

--- adding-interactivity/src/App.tsx
+++ adding-interactivity/src/App.tsx
@@ -1,27 +1,10 @@
-import { MouseEventHandler } from "react";
+import Gallery from './Gallery'
 
-function Button({ onClick, children }: {onClick: MouseEventHandler, children: React.ReactNode}) {
-  return (
-    <button onClick={e => {
-      e.stopPropagation();
-      onClick(e);
-    }}>
-      {children}
-    </button>
-  );
-}
-
-export default function Toolbar() {
-  return (
-    <div className="Toolbar" onClick={() => {
-      alert('You clicked on the toolbar!');
-    }}>
-      <Button onClick={() => alert('Playing!')}>
-        Play Movie
-      </Button>
-      <Button onClick={() => alert('Uploading!')}>
-        Upload Image
-      </Button>
-    </div>
-  );
+export default function Page() {
+    return (
+        <div className='Page'>
+            <Gallery />
+            <Gallery />
+        </div>
+    )
 }
