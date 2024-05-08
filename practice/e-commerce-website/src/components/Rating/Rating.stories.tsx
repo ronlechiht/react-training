6e60385463b6ffdd3981d20262a1173816397f62
ronlechiht <ronlechi1998ht@gmail.com>
2024-05-08 16:21:04 +0700

    feat: implement rating star component

---
+++ practice/e-commerce-website/src/components/Rating/Rating.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Rating from './Rating';
+
+const meta: Meta<typeof Rating> = {
+  component: Rating,
+};
+
+export default meta;
+type Story = StoryObj<typeof Rating>;
+
+export const FirstStory: Story = {
+    args: {
+        rating: 5,
+    },
+  };
---
+++ practice/e-commerce-website/src/components/Rating/Rating.tsx
@@ -0,0 +1,7 @@
+import './Rating.css'
+
+export default function Rating({rating}: {rating: number}) {
+    return (
+        <div className="rating-stars" style={{ "--rating": rating } as React.CSSProperties}></div>
+    )
+}
