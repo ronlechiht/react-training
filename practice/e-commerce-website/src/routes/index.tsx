61e811967117ff185c63e116805cdeb22de736ec
ronlechiht <ronlechi1998ht@gmail.com>
2024-05-22 17:25:38 +0700

    Implement product details page

--- practice/e-commerce-website/src/routes/index.tsx
+++ practice/e-commerce-website/src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from '../layouts';
 import HomePage from '../pages/HomePage';
+import ProductDetailsPage from '../pages/ProductDetailsPage';
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/details/:productId" element={<ProductDetailsPage/>} />
       </Route>
     </Routes>
   );
